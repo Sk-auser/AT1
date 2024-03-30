16ee8178f74fdd76cd20aabe18af9e735304fdd6
S-kauser <saba.kauser@student.uts.edu.au>
2024-03-30 13:06:21 +1100

    gif added

--- eduprod/static/eduprod/scripts.js
+++ eduprod/static/eduprod/scripts.js
@@ -3,18 +3,20 @@ document.addEventListener("DOMContentLoaded", function() {
     const questions = JSON.parse(document.getElementById('content').getAttribute('data-questions'));
     const content = document.getElementById('content');
     const btn = document.getElementById('revealBtn');
+    const noQuestionsMessage = document.getElementById('noQuestionsMessage'); // Select the element
 
     function displayQuestion() {
         if (currentQuestionIndex < questions.length) {
             const question = questions[currentQuestionIndex].fields.question_text;
             const answer = questions[currentQuestionIndex].fields.answer_text;
-            content.innerHTML = `<div class='question'>Country: ${question}</div><div class='answer' style='display: none;'>Capital City: ${answer}</div>`;
+            content.innerHTML = `<div class='question'>Country:  ${question}</div><div class='answer' style='display: none;'>Capital City:  ${answer}</div>`;
             btn.textContent = "Reveal Answer";
             noQuestionsMessage.style.display = "none";
         } else {
             content.innerHTML = ""; // Clear content
             btn.style.display = "none";
             noQuestionsMessage.style.display = "block"; // Display no more questions message
+            gwGif.style.display = "block"; // Display the GIF
         }
     
     }
@@ -31,5 +33,4 @@ document.addEventListener("DOMContentLoaded", function() {
             displayQuestion();
         }
     });
-
-}); // This is the missing closing curly brace
+});
